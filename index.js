28bbfca0010f3b76b501fc9a214e121f38b9e90c
azizderbel <mohamedaziz.derbel@esprit.tn>
2021-02-23 15:11:03 +0100

    2 draft

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 var mongoose= require('mongoose');
 //const PORT =3000;
-var PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
 const app = express();
 
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.promn.mongodb.net/PIDB',{useNewUrlParser: true, useUnifiedTopology: true});
+/*mongoose.connect('mongodb+srv://admin:admin@cluster0.promn.mongodb.net/PIDB',{useNewUrlParser: true, useUnifiedTopology: true});
 var db= mongoose.connection;
 
 db.once('open',function(){
 console.log("Connected to mongoDB Cloud");
-});
-
-app.get('/hello',(req,res,next)=>{
+});*/
+app.use(express.static(__dirname));
+app.get('/',(req,res,next)=>{
     res.send("Hi webservice");
     });
 
 
 
-app.listen(PORT,()=>{console.log('Server is runing on port',PORT);});
+app.listen(port,()=>{console.log('Server is runing on port',port);});
