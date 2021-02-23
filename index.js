3d476d8a5889627b06c7680432b86990083808f8
azizderbel <mohamedaziz.derbel@esprit.tn>
2021-02-23 15:34:31 +0100

    3  draft

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var mongoose= require('mongoose');
 //const PORT =3000;
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 5000;
 const app = express();
 
 
