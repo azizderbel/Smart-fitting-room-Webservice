503cf05e2a3312619b362c047bc3bcb8825ed739
azizderbel <mohamedaziz.derbel@esprit.tn>
2021-02-23 17:38:22 +0100

    1.1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 var express = require('express');
-
+var mongoose= require('mongoose');
 let port = process.env.PORT || 3003;
 
 var app = express();
+mongoose.connect('mongodb+srv://admin:admin@cluster0.promn.mongodb.net/PIDB',{useNewUrlParser: true, useUnifiedTopology: true});
+var db= mongoose.connection;
 
+db.once('open',function(){
+console.log("Connected to mongoDB Cloud");
+});
 
 
 app.get('/',(req,res,next)=>{
